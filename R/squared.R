14c71f3128478b34efe6f0d7731138d60b6328fc
apieters1 <pieters1@student.ubc.ca>
2024-10-29 10:20:43 -0700

    Initial commit

---
+++ R/squared.R
@@ -0,0 +1,15 @@
+#' Squares a number vector
+#'
+#' @param x a number to be squared
+#'
+#' @return the square of a number
+#' @export
+#'
+#' @examples
+#' squared(3)
+#' squared(1:3)
+#'
+squared <- function(x){
+  purrr::map_dbl(x, function(y) y^2)
+}
+
